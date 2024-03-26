b10dd96caf4bacc4a96aef3736baf1d96c0db004
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-03-26 15:14:04 +0800

    stop kidding

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,10 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// Kept for silly windows
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-use tauri::{CustomMenuItem, Menu, Submenu}; // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -26,22 +24,8 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
-    let license = CustomMenuItem::new("license".to_string(), "Lincense Info");
-    let about = Submenu::new("About", Menu::new().add_item(license));
-    let menu = Menu::new()
-        // .add_native_item(MenuItem::Copy) // doesn't work
-        .add_submenu(about);
-
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .menu(menu)
-        .on_menu_event(|event| match event.menu_item_id() {
-            "license" => {
-                std::process::exit(0);
-            }
-            _ => {}
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import JournalView from "./components/JournalView.vue";
+import { ref } from "vue";
+const license = ref(false);
 </script>
 
 <template>
+  <header>
+    <button @click="license = !license">About -> Lisence</button>
+  </header>
   <JournalView />
+  <object data="../LICENSE" style="width: 100%" v-if="license"></object>
 </template>
 
 <style>
