65ac871b55a77ea1bfe327703f1d639187a57620
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-03-26 12:20:32 +0800

    real file grp

--- src/components/FileGroup.vue
+++ src/components/FileGroup.vue
@@ -1,9 +1,15 @@
 <template>
-  <FileSingle v-for="file in files" :key="file" :filename="file" />
+  <div>
+    <button v-for="file in files" :key="file" @click="setFile(file)">
+      {{ file }}
+    </button>
+    <FileSingle v-for="file in uniq(files)" :key="file" v-show="file === currentFilename" :filename="file" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from "vue";
+import { uniq } from "lodash-es";
 import FileSingle from "./FileSingle.vue";
 
 const props = defineProps({
@@ -12,6 +18,13 @@ const props = defineProps({
     required: true,
   },
 });
+
+const currentFilename = ref(props.files[0]); // whatif empty? should be some greeting
+
+function setFile(file) {
+  currentFilename.value = file;
+}
 </script>
 
 <style scoped></style>
+<!-- should overflow x or something, and button thing -->
