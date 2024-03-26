79df094453770d8a607468a886afaf93b757fbbd
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-03-26 14:41:38 +0800

    add lincense info

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,23 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// xyloq, markdown viewer and journal maker from the wonderland
+// Copyright (C) 2024  AmourAmer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Kept for silly windows
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{CustomMenuItem, Menu, Submenu}; // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -8,8 +26,22 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    let license = CustomMenuItem::new("license".to_string(), "Lincense Info");
+    let about = Submenu::new("About", Menu::new().add_item(license));
+    let menu = Menu::new()
+        // .add_native_item(MenuItem::Copy) // doesn't work
+        .add_submenu(about);
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "license" => {
+                std::process::exit(0);
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
